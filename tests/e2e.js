7586bd6e91f09d5d802fdbf90467549c74b2db8e
Aleksandra M. Kurdelska <alex.kurdelska@gmail.com>
2024-05-16 17:22:31 +0200

    Update e2e.js

--- tests/e2e.js
+++ tests/e2e.js
@@ -1,6 +1,6 @@
 import { Selector } from 'testcafe';
 
-fixture("Tests").page("http://http://5.189.170.247:5001/home");
+fixture("Tests").page("http://5.189.170.247:5001/home");
 test("Create Task Test", async (t) => {
     await t.click("#addTask");
     await t.wait(1000);
